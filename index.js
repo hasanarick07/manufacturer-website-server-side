e1179a82999cf85c748e2dae902a0aa9ab72d54b
Arick Hasan <hasanarick000@gmail.com>
2022-05-24 13:21:01 +0600

    mongodb connection done

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const uri =
+  "mongodb+srv://hasanarick:agK4GzcXmKDDRzCX@cluster0.tuis8.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const manufacturerCollection = client.db("manufacturer").collection("tools");
+
+    app.get("/tools", async (req, res) => {
+      const tools = await manufacturerCollection.find({}).toArray();
+      res.send(tools);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   const hi = "hello manufacturer , What's up? arick";
